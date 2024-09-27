4dc4847da1de29f2e1dc8ab4b7be01220151ce67
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-09-27 21:32:53 +0330

    build: drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+    schema: "./src/**/schema.ts",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DB_URL
+    },
+    out: "./drizzle"
+})
