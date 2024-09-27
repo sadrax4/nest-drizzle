f51642c349e3d88239ea16a704d77e73de011d4d
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-09-27 22:21:57 +0330

    build: get users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(
+        private readonly usersService: UsersService
+    ) { }
+
+    @Get()
+    getUsers() {
+        return this.usersService.getUsers();
+    }
+
+}
