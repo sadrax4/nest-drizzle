408c59b4b717f092809328d3625cb8d51a84462d
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-09-28 21:59:41 +0330

    build: user relations

--- src/users/users.schema.ts
+++ src/users/users.schema.ts
@@ -1,4 +1,6 @@
+import { relations } from "drizzle-orm";
 import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { posts } from "src/posts/posts.schema";
 
 export const users = pgTable(
     "users",
@@ -7,4 +9,11 @@ export const users = pgTable(
         email: text("email").unique().notNull(),
         password: text("password").notNull()
     }
+)
+
+export const userRelations = relations(
+    users,
+    ({ many }) => ({
+        posts: many(posts)
+    })
 )
